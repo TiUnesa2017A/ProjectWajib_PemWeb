297f9eb047606d1b8a8195d6d7b84b5393807b3a
Husni Mubarok <44067642+HusniMu@users.noreply.github.com>
2019-05-23 13:54:22 +0700

    initial commit

---
+++ Tugas Wajib 1 (PDO)/delete.php
@@ -0,0 +1,8 @@
+<?php 
+include_once('config.php');
+if(isset($_REQUEST['delId']) and $_REQUEST['delId']!=""){
+	$db->delete('users',array('id'=>$_REQUEST['delId']));
+	header('location: index.php?msg=rds');
+	exit;
+}
+?>
