bc55c5da2be5025196beb0b7f7933e0c4207741f
TiUnesa2017A <50898659+TiUnesa2017A@users.noreply.github.com>
2019-05-27 15:09:47 +0700

    Delete delete.php

--- CRUD_17051204037/delete.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include_once('config.php');
-if(isset($_REQUEST['delId']) and $_REQUEST['delId']!=""){
-	$db->delete('users',array('id'=>$_REQUEST['delId']));
-	header('location: browse-users.php?msg=rds');
-	exit;
-}
-?>
