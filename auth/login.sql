d6e674aa74e9b830c691e3f7b5edba046844bb55
TiUnesa2017A <50898659+TiUnesa2017A@users.noreply.github.com>
2019-05-27 21:26:04 +0700

    first commit

---
+++ auth/login.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 23 Bulan Mei 2019 pada 07.55
+-- Versi server: 10.1.35-MariaDB
+-- Versi PHP: 7.2.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `login`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `loginphp`
+--
+
+CREATE TABLE `loginphp` (
+  `id` int(11) NOT NULL,
+  `fullname` varchar(80) NOT NULL,
+  `email` varchar(80) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `password` text NOT NULL,
+  `type` enum('Administrator','Dosen','Mahasiswa') NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '0',
+  `hash` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `loginphp`
+-
+
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `loginphp`
+--
+ALTER TABLE `loginphp`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`,`username`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `loginphp`
+--
+ALTER TABLE `loginphp`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
